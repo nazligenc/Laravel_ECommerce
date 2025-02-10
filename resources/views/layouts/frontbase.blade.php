15ca54b59587bbef92a3f015d1478b0a51a5f28b
nazligenc <nazli.gencx@gmail.com>
2025-02-10 15:35:38 +0300

    web template implementation

--- resources/views/layouts/frontbase.blade.php
+++ resources/views/layouts/frontbase.blade.php
@@ -1,19 +1,42 @@
 
-<html>
+<!DOCTYPE html>
+<html lang="en">
+
 <head>
-    <title>App Name - @yield('title')</title>
-    @yield('head')
+
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <link href="https://fonts.googleapis.com/css?family=Poppins:100,200,300,400,500,600,700,800,900&display=swap" rel="stylesheet">
+
+    <title>@yield('title')</title>
+
+
+    <!-- Additional CSS Files -->
+    <link rel="stylesheet" type="text/css" href="{{asset('assets')}}/css/bootstrap.min.css">
+
+    <link rel="stylesheet" type="text/css" href="{{asset('assets')}}/css/font-awesome.css">
+
+    <link rel="stylesheet" href="{{asset('assets')}}/css/templatemo-hexashop.css">
+
+    <link rel="stylesheet" href="{{asset('assets')}}/css/owl-carousel.css">
+
+    <link rel="stylesheet" href="{{asset('assets')}}/css/lightbox.css">
+  @yield('head')
 </head>
+
 <body>
-<h1>Header</h1>
+
+@include('home.header')
 @section('sidebar')
-    This is the master sidebar.
+    @include("home.mainbanner")
 @show
 
-<div class="container">
+
     @yield('content')
-</div>
-    <h1>Footer</h1>
+
+    @include('home.footer')
     @yield('foot')
 </body>
 </html>
